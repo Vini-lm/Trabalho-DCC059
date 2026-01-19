ffe7d771bf112278ec379c2ffe45d6dc5ab9a67d
Vini-LM <vinicius.matos@estudante.ufjf.br>
2026-01-18 21:19:46 -0300

    Adiciona funções e atributos para a classe Grafo.

--- Grafo.h
+++ Grafo.h
@@ -1,8 +1,29 @@
 #ifndef TRABALHO_DCC059_GRAFO_H
 #define TRABALHO_DCC059_GRAFO_H
+#include <vector>
 
+using namespace std;
 
 class Grafo {
+
+
+private:
+    int n;
+    vector<vector<int>> adjList;
+    vector<int> colors;
+ 
+
+
+public:
+    Grafo(int nVertices);
+    void addAresta(int u, int v);
+
+    int getNumVertices();
+    int getNumArestas();
+    vector<int>& getVizinhos();
+    int getGrau(int u);
+
+    
 };
 
 
